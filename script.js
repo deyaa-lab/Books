0ef6861033e221e52ede8952b40a67db5d6efe83
deyaaalnouti88 <>
2021-06-03 14:46:39 +0000

    books

---
+++ script.js
@@ -0,0 +1,21 @@
+var userName = prompt("Enter your name ? ");
+
+console.log(userName);
+
+alert("You are now in the right place " + userName)
+
+
+document.write("<p style='color:white;'> welcome " + userName + "</p>")
+
+var userage = prompt("Enter your age ? ");
+
+
+if (userage <= 18) {
+     alert("good")
+}else{
+  alert("you are too young")
+}
+
+var n = prompt("do you like reading ? ");
+document.write("<img src='https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg'> "); 
+
